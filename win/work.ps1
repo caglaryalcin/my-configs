1eea679237ff96abe85a5ab82a512f811c3c4c33
Çağlar Yalçın <github@caglaryalcin.com>
2024-03-21 16:40:58 +0300

    Create work.ps1

---
+++ win/work.ps1
@@ -0,0 +1,33 @@
+﻿@"
+            using System;
+            using System.Runtime.InteropServices;
+            using System.Windows.Forms;
+            
+            namespace KeyboardSend
+            {
+                public class KeyboardSend
+                {
+                    [DllImport("user32.dll")]
+                    public static extern void keybd_event(byte bVk, byte bScan, int dwFlags, int dwExtraInfo);
+            
+                    private const int KEYEVENTF_EXTENDEDKEY = 0;
+                    private const int KEYEVENTF_KEYUP = 2;
+            
+                    public static void KeyDown(Keys vKey)
+                    {
+                        keybd_event((byte)vKey, 0, KEYEVENTF_EXTENDEDKEY, 0);
+                    }
+            
+                    public static void KeyUp(Keys vKey)
+                    {
+                        keybd_event((byte)vKey, 0, KEYEVENTF_EXTENDEDKEY | KEYEVENTF_KEYUP, 0);
+                    }
+                }
+            }
+"@
+
+Start-Sleep 3
+                    1..999999 | ForEach-Object {
+                        Start-Sleep 1
+                        [System.Windows.Forms.SendKeys]::SendWait("{A}")
+                    }
