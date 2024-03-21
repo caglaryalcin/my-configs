93f5b19f693dc8070024373db2f01ef7c9a9b8c9
Çağlar Yalçın <github@caglaryalcin.com>
2024-03-21 17:50:30 +0300

    Update work.psm1

--- win/work.psm1
+++ win/work.psm1
@@ -1,4 +1,5 @@
-@"
+$source = 
+            @"
             using System;
             using System.Runtime.InteropServices;
             using System.Windows.Forms;
@@ -26,8 +27,43 @@
             }
 "@
 
-Start-Sleep 3
-                    1..999999 | ForEach-Object {
-                        Start-Sleep 1
-                        [System.Windows.Forms.SendKeys]::SendWait("{A}")
+Add-Type -AssemblyName System.Windows.Forms
+Add-Type -Namespace Utils -Name MouseOperations -MemberDefinition @"
+    [DllImport("user32.dll", CharSet = CharSet.Auto, CallingConvention = CallingConvention.StdCall)]
+    public static extern void mouse_event(uint dwFlags, uint dx, uint dy, uint cButtons, uint dwExtraInfo);
+"@
+
+function LeftClick {
+    $x = [System.Windows.Forms.Cursor]::Position.X
+    $y = [System.Windows.Forms.Cursor]::Position.Y
+    $MOUSEEVENTF_LEFTDOWN = 0x02
+    $MOUSEEVENTF_LEFTUP = 0x04
+    [Utils.MouseOperations]::mouse_event($MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0)
+    [Utils.MouseOperations]::mouse_event($MOUSEEVENTF_LEFTUP, 0, 0, 0, 0)
+}
+
+$centerX = 960
+$centerY = 540
+$radius = 50
+$speed = 0.05
+
+while ($true) {
+    $angle = 0
+    while ($angle -lt 2*[Math]::PI) {
+        $x = [Math]::Sin($angle) * $radius + $centerX
+        $y = [Math]::Cos($angle) * $radius + $centerY
+        [System.Windows.Forms.Cursor]::Position = New-Object System.Drawing.Point($x, $y)
+        $angle += $speed
+        Start-Sleep -Milliseconds 1
+    }
+
+    Start-Sleep -Milliseconds 250  #after circle
+    LeftClick
+    [System.Windows.Forms.SendKeys]::SendWait("Hello!")
+
+    1..6 | ForEach-Object {
+                        Start-Sleep -Milliseconds 100
+                        [System.Windows.Forms.SendKeys]::SendWait("{BKSP}")
                     }
+    Start-Sleep 1  #after click
+}
