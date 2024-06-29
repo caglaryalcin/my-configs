41c228a1b866019d0840a3dc8056ba8fff8de613
Caglar Yalcin <github@caglaryalcin.com>
2024-06-29 19:24:43 +0300

    fix

--- remote-code/nvidia.psm1
+++ remote-code/nvidia.psm1
@@ -1,36 +1,48 @@
 # Nvidia Profile Inspector
 $nvidiainspector = "https://github.com/Orbmu2k/nvidiaProfileInspector/releases/download/2.4.0.4/nvidiaProfileInspector.zip"
-$nvidiazippath = "c:\nvidiaProfileInspector.zip"
+$nvidiazippath = "C:\nvidiaProfileInspector.zip"
+$nvidiainspectorpath = "C:\Program Files\nvidiaProfileInspector"
 $nvidiaconfigpath = "C:\Program Files\nvidiaProfileInspector\Base-Profile.nip"
 $nvidiaconfigurl = "https://github.com/caglaryalcin/my-configs/raw/main/hardware/nvidia/Base-Profile.nip"
 
-# Download the Nvidia Profile Inspector
-Invoke-WebRequest -Uri $nvidiainspector -Outfile $nvidiazippath
+function DownloadAndExtract {
+    # Download the Nvidia Profile Inspector
+    Invoke-WebRequest -Uri $nvidiainspector -Outfile $nvidiazippath
 
-# Extract the Nvidia Profile Inspector
-$nvidiaInspectorpath = "C:\Program Files\nvidiaProfileInspector"
+    # Create the folder for Nvidia Profile Inspector
+    New-Item -Path $nvidiainspectorpath -ItemType Directory -Force
 
-if (-not (Test-Path -Path $nvidiaInspectorpath -PathType Container)) {
-    & 'C:\Program Files\7-Zip\7z.exe' x $nvidiazippath -o'C:\Program Files\nvidiaProfileInspector' *>$null
+    # Extract the Nvidia Profile Inspector
+    & 'C:\Program Files\7-Zip\7z.exe' x $nvidiazippath -o"$nvidiainspectorpath" *>$null
+
+    # Clean up the zip file
+    Remove-Item $nvidiazippath -ErrorAction SilentlyContinue
 }
-else {
-    ##
-}
 
-# Import the base profile
-if (-not (Test-Path -Path $nvidiaconfigpath -PathType Container)) {
+function ImportBaseProfile {
+    # Download the base profile
     Invoke-WebRequest -Uri $nvidiaconfigurl -Outfile $nvidiaconfigpath
-    Set-Location "C:\Program Files\nvidiaProfileInspector"
+
+    # Import the base profile
+    Set-Location $nvidiainspectorpath
     .\nvidiaProfileInspector.exe -silentImport .\Base-Profile.nip
     Start-Sleep 1
 }
-else {
-    ##
+
+function KillNvidiaProfileInspector {
+    # Kill the Nvidia Profile Inspector process if running
+    taskkill.exe /f /im nvidiaProfileInspector.exe *>$null
 }
 
-# Kill the Nvidia Profile Inspector
-taskkill.exe /f /im nvidiaProfileInspector.exe *>$null
+# Check if the Nvidia Profile Inspector folder exists
+if (Test-Path -Path $nvidiainspectorpath -PathType Container) {
+    # Folder exists, delete it and its contents
+    Remove-Item -Path $nvidiainspectorpath -Recurse -Force
+}
 
-# Clean up the files
-Set-Location "c:\"
-Remove-Item "c:\nvidiaProfileInspector.zip" -Recurse -ErrorAction SilentlyContinue
+# Download, extract, and import base profile
+DownloadAndExtract
+ImportBaseProfile
+
+# Kill any running instance of Nvidia Profile Inspector
+KillNvidiaProfileInspector
